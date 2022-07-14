63d51dc5b23c6be7ce79906ceb035cba28faf8a5
komplektan <komplektan@mail.ru>
2022-07-14 18:45:08 +0500

    TwoButton

---
+++ CustomButtons/IconTwoButton.swift
@@ -0,0 +1,13 @@
+//
+//  IconTwoButton.swift
+//  CustomButtons
+//
+//  Created by Вячеслав Квашнин on 14.07.2022.
+//
+
+import UIKit
+
+class IconTwoButton: UIButton {
+    
+
+}
--- CustomButtons/ViewController.swift
+++ CustomButtons/ViewController.swift
@@ -11,7 +11,15 @@ class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-    
+        let twoLinedButton = TwoLinedButton(frame: CGRect(x: 0, y: 0,
+                                                          width: 220,
+                                                          height: 55))
+        view.addSubview(twoLinedButton)
+        twoLinedButton.center = view.center
+        twoLinedButton.configure(with: TwoLinedButtonViewModel(
+            primaryText: "Start",
+            secondaryText: "Days free")
+        )
     }
 }
 
